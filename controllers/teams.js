2caa156ba1755594f584f9be7489b7857a7dc22f
carrye <carryegaulin033178@gmail.com>
2020-11-10 20:33:35 -0500

    new team added

--- controllers/teams.js
+++ controllers/teams.js
@@ -4,6 +4,25 @@ const getAllTeams = (request, response) => {
   return response.send(teams)
 }
 
+const saveNewTeam = (request, response) => {
+  const {
+    id, location, mascot, abbreviation, conference, division
+  } = request.body
+
+  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    // eslint-disable-next-line max-len
+    return response.status(400).send('The following fields are required: location, mascot, abbreviation, conference, division')
+  }
+  const newTeam = {
+    // eslint-disable-next-line max-len
+    id: 33, location: 'Louisiana', mascot: 'Tiger', abbreviation: 'LSU', conference: 'Southeastern', division: 'Western'
+  }
+
+  teams.push(newTeam)
+
+  return response.send(newTeam)
+}
+
 const getTeamBySlug = (request, response) => {
   const { id } = request.params
 
@@ -12,6 +31,6 @@ const getTeamBySlug = (request, response) => {
   return response.send(foundTeam)
 }
 
-module.exports = { getTeamBySlug, getAllTeams }
+module.exports = { getTeamBySlug, getAllTeams, saveNewTeam }
 
 
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const { getAllTeams, getTeamBySlug } = require('./controllers/teams')
+const bodyParser = require('body-parser')
+
+const { getAllTeams, getTeamBySlug, saveNewTeam } = require('./controllers/teams')
 const app = express()
 
 app.get('/', getAllTeams)
 
 app.get('/:id', getTeamBySlug)
+
+app.post('/', express.json(), saveNewTeam)
+
 
 app.listen(1338, () => {
   // eslint-disable-next-line no-console
